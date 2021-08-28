35c5c5b06092f7474e38fe8e656faf56de1625f6
roman <romansheikh170@gmail.com>
2021-08-28 18:20:34 +0600

    --- Auto-Git Commit ---

--- js/meal.js
+++ js/meal.js
@@ -11,11 +11,7 @@ const displayAllMeals = meals => {
     let allMeals = meals.meals;
     let mealsContainer = document.getElementById('meals-container');
     allMeals.forEach(meal => {
-        console.log(meal.strMealThumb);
-        console.log(meal.strMeal);
         let mealDisc = meal.strInstructions
-        console.log(mealDisc.slice(0, 125));
-
         let mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
@@ -24,7 +20,7 @@ const displayAllMeals = meals => {
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
                     <p class="card-text">${mealDisc.slice(0, 125)}</p>
-                    <button class="btn btn-outline-warning fw-bolder">Learn More</button>
+                    <button class="btn btn-outline-warning fw-bolder" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="displayMealInfo('${meal.strMeal}')">Learn More</button>
                 </div>
             </div>
         `
@@ -32,4 +28,21 @@ const displayAllMeals = meals => {
 
         
     })
+}
+
+
+function displayMealInfo(name){
+    // console.log(name);
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    fetch(url)
+    .then(ref => ref.json())
+    .then(data => displayMealDetails(data))
+}
+
+function displayMealDetails(meals){
+    // console.log(meals.meals);
+    let meal = meals.meals[0];
+    let mealTitle = document.getElementById('meal-title');
+    // mealTitle.innerText = `${meal}`
+    console.log(meal.strMeal);
 }
