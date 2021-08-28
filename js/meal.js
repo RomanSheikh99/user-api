d6e648b2e33a50d794dd444a5f78955c1c472fb2
roman <romansheikh170@gmail.com>
2021-08-28 11:29:43 +0600

    --- Auto-Git Commit ---

--- js/meal.js
+++ js/meal.js
@@ -8,7 +8,27 @@ const loadMeals = () => {
 loadMeals();
 
 const displayAllMeals = meals => {
-    meals.forEach( meal => {
-        console.log(meal);
-    });
+    let allMeals = meals.meals;
+    let mealsContainer = document.getElementById('meals-container');
+    allMeals.forEach(meal => {
+        console.log(meal.strMealThumb);
+        console.log(meal.strMeal);
+        let mealDisc = meal.strInstructions
+        console.log(mealDisc.slice(0, 125));
+
+        let mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${mealDisc.slice(0, 125)}</p>
+                </div>
+            </div>
+        `
+        mealsContainer.appendChild(mealDiv);
+
+        
+    })
 }
